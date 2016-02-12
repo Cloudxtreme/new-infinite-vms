f346195dbf342d1535d94709e20852092bf3259b
Infinite.sx <ops@shift.sx>
2016-02-12 21:57:28 -0200

    Update sysctl-config.sh

--- sysctl-config.sh
+++ sysctl-config.sh
@@ -6,4 +6,14 @@ echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> /etc/sysctl.conf
 echo "kernel.sysrq=0" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_sack=0" >> /etc/sysctl.conf
 sysctl -e -p
+service cups stop
+chkconfig cups off
+service nfslock stop
+chkconfig nfslock off
+service rpcidmapd stop
+chkconfig rpcidmapd off
+service portreserve stop
+chkconfig portreserve off
+service rpcbind stop
+chkconfig rpcbind off
 EOF
