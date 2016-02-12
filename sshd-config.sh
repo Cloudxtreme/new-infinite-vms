803ab6f6f7b3c48c406bfa596feb66bea9fc44f1
Infinite.sx <ops@shift.sx>
2016-02-12 21:32:33 -0200

    Create sshd-config.sh

---
+++ sshd-config.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sed -i 's/PermitRootLogin no/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/#RSAAuthentication yes/RSAAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#GatewayPorts no/GatewayPorts yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+/etc/init.d/sshd restart
+EOF
