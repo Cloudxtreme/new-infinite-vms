cc0f315160e5c16432f3eac77d67b1ba9537f32c
Infinite.sx <ops@shift.sx>
2016-02-12 21:56:10 -0200

    Create sysctl-config.sh

---
+++ sysctl-config.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "kernel.panic=10" >> /etc/sysctl.conf
+echo "kernel.user_ptrace=0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.accept_source_route=0" >> /etc/sysctl.conf
+echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> /etc/sysctl.conf
+echo "kernel.sysrq=0" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_sack=0" >> /etc/sysctl.conf
+EOF
