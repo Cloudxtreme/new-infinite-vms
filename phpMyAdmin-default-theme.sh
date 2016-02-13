3cc2410085e95d13933a0cc745676007185f0100
Infinite.sx <ops@shift.sx>
2016-02-12 23:58:02 -0200

    Create phpMyAdmin-default-theme.sh

---
+++ phpMyAdmin-default-theme.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo -n "$" >> /usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
+echo -n "cfg['ThemeDefault']= 'infinite';" >> /usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
+EOF
