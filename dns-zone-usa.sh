7c4f6394024e2e0b4c3d8fb7fed77ad24162275b
Infinite.sx <ops@shift.sx>
2016-02-14 17:23:25 -0200

    Create dns-zone-usa.sh

---
+++ dns-zone-usa.sh
@@ -0,0 +1,4 @@
+mv -f /root/infinite-scripts/usa/root_simple /var/cpanel/zonetemplates/root_simple
+mv -f /root/infinite-scripts/usa/root_standard /var/cpanel/zonetemplates/root_standard
+mv -f /root/infinite-scripts/usa/root_standardvirtualftp /var/cpanel/zonetemplates/root_standardvirtualftp
+rmdir /root/infinite-scripts/usa
