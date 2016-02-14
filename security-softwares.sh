1193e68090c006527469de97634a08fd7a38314c
Infinite.sx <ops@shift.sx>
2016-02-14 16:42:16 -0200

    Update security-softwares.sh

--- security-softwares.sh
+++ security-softwares.sh
@@ -9,7 +9,7 @@ ln -s /usr/local/cpanel/3rdparty/bin/clamscan /usr/local/bin/clamscan
 ln -s /usr/local/cpanel/3rdparty/bin/freshclam /usr/local/bin/freshclam
 sed -i 's/email_alert="0"/email_alert="1"/g' /usr/local/maldetect/conf.maldet
 sed -i 's/email_addr="you@domain.com"/email_addr="root@localhost"/g' /usr/local/maldetect/conf.maldet
-sed -i 's/scan_max_filesize="768k"/scan_max_filesize="1536/g' /usr/local/maldetect/conf.maldet
+sed -i 's/scan_max_filesize="768k"/scan_max_filesize="1536k"/g' /usr/local/maldetect/conf.maldet
 sed -i 's/quarantine_hits="0"/quarantine_hits="1"/g' /usr/local/maldetect/conf.maldet
 sed -i 's/quarantine_clean="0"/quarantine_clean="1"/g' /usr/local/maldetect/conf.maldet
 
