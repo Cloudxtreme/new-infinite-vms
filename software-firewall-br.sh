f0c11247cf6a5f9b72253a53c7847611aeca9acb
Infinite.sx <ops@shift.sx>
2016-02-14 17:32:10 -0200

    Create software-firewall-br.sh

---
+++ software-firewall-br.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd /usr/src
+rm -fv csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
+perl /usr/local/csf/bin/csftest.pl
+yum install perl-GDGraph
