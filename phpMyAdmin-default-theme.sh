28bfddbc9e8a38000ff5b7358912e5d1ae253646
Infinite.sx <ops@shift.sx>
2016-02-14 15:51:20 -0200

    Update phpMyAdmin-default-theme.sh

--- phpMyAdmin-default-theme.sh
+++ phpMyAdmin-default-theme.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+$DIRECTORY=/usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite
+if [ ! -d "$DIRECTORY" ]; then
 mv /root/infinite-scripts/infinite-phpmyadmin.tar.gz /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite-phpmyadmin.tar.gz
-rmdir infinite-phpmyadmin.tar.gz
+tar -zxf /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite-phpmyadmin.tar.gz
+rm /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite-phpmyadmin.tar.gz
 echo -n "$" >> /usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
 echo -n "cfg['ThemeDefault']= 'original';" >> /usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
+fi
