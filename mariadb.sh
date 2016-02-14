84dee2c88d92db2f4234a0148b8ab7a8444c6442
Infinite.sx <ops@shift.sx>
2016-02-14 15:40:07 -0200

    Create mariadb.sh

---
+++ mariadb.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+cp -Rf /var/lib/mysql /var/lib/mysql-old
+mv /etc/my.cnf /etc/my.cnf-old 
+/scripts/update_local_rpm_versions --edit target_settings.MySQL50 uninstalled
+/scripts/update_local_rpm_versions --edit target_settings.MySQL51 uninstalled
+/scripts/update_local_rpm_versions --edit target_settings.MySQL55 uninstalled 
+/scripts/update_local_rpm_versions --edit target_settings.MySQL56 uninstalled
+/scripts/check_cpanel_rpms --fix --targets=MySQL50,MySQL51,MySQL55,MySQL56
+mv /root/infinite-scripts/MariaDB.repo /etc/yum.repos.d/MariaDB.repo
+sed -i 's/mysql[*] nsd[*] php/nsd/g' /etc/yum.conf
+yum install MariaDB-server MariaDB-client MariaDB-devel php-mysql
+/etc/init.d/mysql start
+mysql_upgrade
+/etc/init.d/mysql restart
+yum install governor-mysql
+/usr/share/lve/dbgovernor/db-select-mysql --mysql-version=auto
+/usr/share/lve/dbgovernor/mysqlgovernor.py --install
