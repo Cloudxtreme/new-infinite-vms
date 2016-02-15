f11feff1c6590b9baa742209674d09d04bcf1257
Infinite.sx <ops@shift.sx>
2016-02-15 01:41:34 -0200

    Update backup-disk.sh

--- backup-disk.sh
+++ backup-disk.sh
@@ -2,11 +2,11 @@
 mkdir /backup-disk
 mkfs.ext4 -F /dev/sdb
 mount -o discard,defaults /dev/sdb /backup-disk
-echo -n "/swapfile" >> /etc/fstab
+echo -n "/dev/sdb" >> /etc/fstab
 echo -e -n "\t" >> /etc/fstab
-echo -n "swap" >> /etc/fstab
+echo -n "/backup-disk" >> /etc/fstab
 echo -e -n "\t" >> /etc/fstab
-echo -n "swap" >> /etc/fstab
+echo -n "ext4" >> /etc/fstab
 echo -e -n "\t" >> /etc/fstab
 echo -n "discard,defaults" >> /etc/fstab
 echo -e -n "\t" >> /etc/fstab
