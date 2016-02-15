7529e100c4e6566b396434c3d2281be51c85bca3
Infinite.sx <ops@shift.sx>
2016-02-15 01:08:50 -0200

    Update global-header.sh

--- global-header.sh
+++ global-header.sh
@@ -2,4 +2,5 @@
 mv /root/infinite-scripts/global_header.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/global_header.tar.gz
 tar -zvxf /usr/local/cpanel/base/frontend/paper_lantern/global_header.tar.gz -C /usr/local/cpanel/base/frontend/paper_lantern/
 rm -f /usr/local/cpanel/base/frontend/paper_lantern/global_header.tar.gz
+mkdir /var/cpanel/customizations/includes
 mv /usr/local/cpanel/base/frontend/paper_lantern/global_header.html.tt /var/cpanel/customizations/includes/global_header.html.tt
