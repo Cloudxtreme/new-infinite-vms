16a2b3f39ca441bfc648bc1258bfa57e2aa2e43c
Infinite.sx <ops@shift.sx>
2016-02-15 19:49:23 -0200

    Create dns-zone-mail.sh

---
+++ dns-zone-mail.sh
@@ -0,0 +1,4 @@
+mv -f /root/infinite-scripts/mail/root_simple /var/cpanel/zonetemplates/root_simple
+mv -f /root/infinite-scripts/mail/root_standard /var/cpanel/zonetemplates/root_standard
+mv -f /root/infinite-scripts/mail/root_standardvirtualftp /var/cpanel/zonetemplates/root_standardvirtualftp
+rmdir /root/infinite-scripts/mail
