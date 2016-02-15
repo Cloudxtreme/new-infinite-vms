66af13070c5c7de59feabd4ae79d621c1a770d90
Infinite.sx <ops@shift.sx>
2016-02-15 19:53:53 -0200

    Delete dns-zone-mail.sh

--- dns-zone-mail.sh
+++
@@ -1,4 +0,0 @@
-mv -f /root/infinite-scripts/mail/root_simple /var/cpanel/zonetemplates/root_simple
-mv -f /root/infinite-scripts/mail/root_standard /var/cpanel/zonetemplates/root_standard
-mv -f /root/infinite-scripts/mail/root_standardvirtualftp /var/cpanel/zonetemplates/root_standardvirtualftp
-rmdir /root/infinite-scripts/mail
