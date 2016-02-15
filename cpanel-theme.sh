898af37c234a0a3b3a590ef515cf625370132fd8
Infinite.sx <ops@shift.sx>
2016-02-14 22:35:37 -0200

    Create cpanel-theme.sh

---
+++ cpanel-theme.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+mkdir /var/cpanel/customizations/styled
+mv /root/infinite-scripts/temas-infinite.tar.gz /var/cpanel/customizations/styled/temas-infinite.tar.gz
+tar -zxf /var/cpanel/customizations/styled/temas-infinite.tar.gz
+rm /var/cpanel/customizations/styled/temas-infinite.tar.gz
+ln -s /var/cpanel/customizations/styled/Original default_style
