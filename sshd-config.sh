60006e684caafc2d9c2a3ec76bd2997bd9ce3a89
Infinite.sx <ops@shift.sx>
2016-02-16 13:17:59 -0200

    Update sshd-config.sh

--- sshd-config.sh
+++ sshd-config.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 sed -i 's/PermitRootLogin no/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin without-password/g' /etc/ssh/sshd_config
 sed -i 's/#RSAAuthentication yes/RSAAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/#GatewayPorts no/GatewayPorts yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
