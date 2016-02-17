50029f6a8bf8a1200484318af7be3e8bfa816316
Infinite.sx <ops@shift.sx>
2016-02-16 22:14:03 -0200

    Update sshd-config.sh

--- sshd-config.sh
+++ sshd-config.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i 's/#AddressFamily any/Port 2222/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin no/PermitRootLogin without-password/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin yes/PermitRootLogin without-password/g' /etc/ssh/sshd_config
 sed -i 's/#RSAAuthentication yes/RSAAuthentication yes/g' /etc/ssh/sshd_config
