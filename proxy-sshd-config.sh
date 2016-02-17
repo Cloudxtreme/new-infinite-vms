4ad63d0a543ace1930f4c0b5aaec0a8c0922bb39
Infinite.sx <ops@shift.sx>
2016-02-17 21:24:42 -0200

    Update proxy-sshd-config.sh

--- proxy-sshd-config.sh
+++ proxy-sshd-config.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 sed -i 's/#Port 22/Port 2221/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin no/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
