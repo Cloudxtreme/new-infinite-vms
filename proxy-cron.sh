806dd12c8eb9dc43d1796baf424a67222622bc28
Infinite.sx <ops@shift.sx>
2016-02-17 21:53:44 -0200

    Create proxy-cron.sh

---
+++ proxy-cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum install cronie
+service crond start
+chkconfig crond on
+touch /var/spool/cron/root
+echo "00 22 */3 * * /usr/local/maldetect/maldet -d" | tee -a /var/spool/cron/root
+echo "10 22 */3 * * /usr/local/maldetect/maldet -u" | tee -a /var/spool/cron/root
+echo "30 22 */3 * * /usr/local/maldetect/maldet -a" | sudo tee -a /var/spool/cron/root
+echo "00 23 1,15 * * (cd /root/chkrootkit-0.50; ./chkrootkit 2>&1)" | tee -a /var/spool/cron/root
+echo "*/1 * * * * sh /root/infinite-scripts/proxy-curl-check.sh >> /dev/null" | tee -a /var/spool/cron/root
