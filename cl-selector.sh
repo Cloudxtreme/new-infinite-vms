4d19c5be5f07266ef09b28508ab941b0d43f30fb
Infinite.sx <ops@shift.sx>
2016-02-17 21:27:06 -0200

    Update cl-selector.sh

--- cl-selector.sh
+++ cl-selector.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 sed -i 's/Default   = 30/Default   = 60/g' /etc/cl.selector/php.conf
 sed -i 's/Range     = 64M,128M,192M,256M,368M,512M/Range     = 64M,128M,256M,368M,512M,1G/g' /etc/cl.selector/php.conf
 sed -i 's/Default   = 8M/Default   = 16M/g' /etc/cl.selector/php.conf
