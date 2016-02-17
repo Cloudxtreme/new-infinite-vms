69dd73c2e811f0b1cc58a2e2000e31aa518bd9b8
Infinite.sx <ops@shift.sx>
2016-02-17 21:28:03 -0200

    Create proxy-curl-check.sh

---
+++ proxy-curl-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+nginx_conf=/etc/nginx/nginx.conf
+curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 3)
+curl_result=200
+main_proxy_pass="proxy_pass http://ip-cloud;"
+cp_proxy_pass="proxy_pass http://cpanel-webserver;"
+main_return="return 503; #main"
+cp_return="return 503; #cp"
+
+if [ "$curl_check" != "$curl_result" ]; then
+sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf;
+sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf;
+else
+sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_conf;
+sed -r -i -e "s|$cp_return|$cp_proxy_pass|g" $nginx_conf;
+fi
