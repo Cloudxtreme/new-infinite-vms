72440f73d90b1d065161d8dc2f2c39ff05e4994d
Infinite.sx <ops@shift.sx>
2016-02-17 21:23:08 -0200

    Update lsapi.sh

--- lsapi.sh
+++ lsapi.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 yum install liblsapi liblsapi-devel --enablerepo=cloudlinux-updates-testing
 yum install cpanel-mod-lsapi --enablerepo=cloudlinux-updates-testing
 /usr/bin/switch_mod_lsapi --enable-global
