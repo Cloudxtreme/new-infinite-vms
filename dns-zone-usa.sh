80446e367002843062743a838fee5178444a1532
Infinite.sx <ops@shift.sx>
2016-02-17 21:22:38 -0200

    Update dns-zone-usa.sh

--- dns-zone-usa.sh
+++ dns-zone-usa.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mv -f /root/infinite-scripts/usa/root_simple /var/cpanel/zonetemplates/root_simple
 mv -f /root/infinite-scripts/usa/root_standard /var/cpanel/zonetemplates/root_standard
 mv -f /root/infinite-scripts/usa/root_standardvirtualftp /var/cpanel/zonetemplates/root_standardvirtualftp
