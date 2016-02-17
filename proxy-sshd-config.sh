466c65a9fa1c3dd56866d009f0ced7c825e55dff
Infinite.sx <ops@shift.sx>
2016-02-16 22:44:00 -0200

    Create proxy-sshd-config.sh

---
+++ proxy-sshd-config.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sed -i 's/#Port 22/Port 2221/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin no/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i 's/#GatewayPorts no/GatewayPorts yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/# %wheel        ALL=(ALL)	ALL/%wheel        ALL=(ALL)	ALL/g' /etc/sudoers
+/etc/init.d/sshd restart
