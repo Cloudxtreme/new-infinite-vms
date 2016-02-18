9b0a31f2177bbc3a4cb4ed2147e794caf3a3b48f
Infinite.sx <ops@shift.sx>
2016-02-18 11:38:50 -0200

    Update main-cron.sh

--- main-cron.sh
+++ main-cron.sh
@@ -14,4 +14,3 @@ echo "0 4 * * * mysqlcheck -Aos -u root > /dev/null" | tee -a /var/spool/cron/ro
 echo "30 0 * * 0 find /home/ -type f -name error_log -delete" | tee -a /var/spool/cron/root
 echo "* 0,6,12,18 * * * sync; echo 3 > /proc/sys/vm/drop_caches" | tee -a /var/spool/cron/root
 echo "30 1 1,15 * * yum clean all" | tee -a /var/spool/cron/root
-echo "0 2 1,15 * * reboot" | tee -a /var/spool/cron/root
