b7b667f2a49f332126fe1d7e80ddde71e3fd95ec
Infinite.sx <ops@shift.sx>
2016-02-17 22:18:34 -0200

    Update proxy-curl-check.sh

--- proxy-curl-check.sh
+++ proxy-curl-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nginx_conf=/etc/nginx/nginx.conf
-curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 3)
+curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 30)
 curl_result=200
 main_proxy_pass="proxy_pass http://ip-cloud;"
 cp_proxy_pass="proxy_pass http://cpanel-webserver;"
@@ -8,9 +8,14 @@ main_return="return 503; #main"
 cp_return="return 503; #cp"
 
 if [ "$curl_check" != "$curl_result" ]; then
-sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf;
+sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf; && service nginx reload
 sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf;
 else
 sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_conf;
 sed -r -i -e "s|$cp_return|$cp_proxy_pass|g" $nginx_conf;
 fi
+
+while inotifywait -q -e modify $nginx_conf >/dev/null; do
+    service nginx stop
+    service nginx start
+done
