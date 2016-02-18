3f1e43d8e71a6c7cd123355c539bc9e8bb8ec450
Infinite.sx <ops@shift.sx>
2016-02-18 01:00:39 -0200

    Update mail-csf.sh

--- mail-csf.sh
+++ mail-csf.sh
@@ -6,6 +6,7 @@ sed -i 's/TCP_OUT = "20,21,22,25,37,43,53,80,110,113,443,587,873,993,995,2086,20
 sed -i 's/UDP_IN = "20,21,53"/UDP_IN = "465,587,783,6277,24441"/g' /etc/csf/csf.conf
 sed -i 's/UDP_OUT = "20,21,53,113,123,873,6277,24441"/UDP_OUT = "465,587,6277,24441"/g' /etc/csf/csf.conf
 sed -i 's/ICMP_IN = "1"/ICMP_IN = "0"/g' /etc/csf/csf.conf
+sed -i 's/IPV6 = "0"/IPV6 = "1"/g' /etc/csf/csf.conf
 sed -i 's/IPV6_ICMP_STRICT = "0"/IPV6_ICMP_STRICT = "1"/g' /etc/csf/csf.conf
 sed -i 's/TCP6_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,2077,2078,2082,2083,2086,2087,2095,2096"/TCP6_IN = ""/g' /etc/csf/csf.conf
 sed -i 's/TCP6_OUT = "20,21,22,25,37,43,53,80,110,113,443,587,873,993,995,2086,2087,2089,2703"/TCP6_OUT = ""/g' /etc/csf/csf.conf
