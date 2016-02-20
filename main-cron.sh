f6fcb902a1a40af00f6d7db59b9889139404f19b
Infinite.sx <ops@shift.sx>
2016-02-20 03:14:16 -0200

    Update main-cron.sh

--- main-cron.sh
+++ main-cron.sh
@@ -7,11 +7,11 @@ echo "20 22 * * * /usr/local/maldetect/maldet -b -r /home?/?/public_html 3" | su
 echo "*/15 * * * * /usr/local/maldetect/maldet -b -r /home?/?/public_html 1 > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "0 22 1,15 * * (cd /root/chkrootkit-0.50; ./chkrootkit 2>&1)" | tee -a /var/spool/cron/root
 echo "*/2 * * * * /usr/local/sim/bin/sim -q >> /dev/null" | tee -a /var/spool/cron/root
-echo "* 0 * * * sh /root/infinite-scripts/whm-theme-changer.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
-echo "* 0 * * * sh /root/infinite-scripts/filemanager-theme-changer.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
-echo "* 0 * * * sh /root/infinite-scripts/phpMyAdmin-default-theme.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
+echo "7 0 * * * sh /root/infinite-scripts/whm-theme-changer.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
+echo "6 0 * * * sh /root/infinite-scripts/filemanager-theme-changer.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
+echo "5 0 * * * sh /root/infinite-scripts/phpMyAdmin-default-theme.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "0 4 * * * mysqlcheck -Aos -u root > /dev/null" | tee -a /var/spool/cron/root
 echo "30 0 * * 0 find /home/ -type f -name error_log -delete" | tee -a /var/spool/cron/root
-echo "* 0,6,12,18 * * * sync; echo 3 > /proc/sys/vm/drop_caches" | tee -a /var/spool/cron/root
+echo "0 0,6,12,18 * * * sync; echo 3 > /proc/sys/vm/drop_caches" | tee -a /var/spool/cron/root
 echo "30 1 1,15 * * yum clean all" | tee -a /var/spool/cron/root
-echo "0,30 0,6,12,18 * * * /scripts/dnscluster syncall" | tee -a /var/spool/cron/root
+echo "0 0,6,12,18 * * * /scripts/dnscluster syncall" | tee -a /var/spool/cron/root
