7845ca6676313a381f06cfa3ad46d474d7951f51
Infinite.sx <ops@shift.sx>
2016-02-20 13:10:35 -0200

    Update phpMyAdmin-default-theme.sh

--- phpMyAdmin-default-theme.sh
+++ phpMyAdmin-default-theme.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-$DIRECTORY=/usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite
-if [ ! -d "$DIRECTORY" ]; then
-cp -u /root/infinite-scripts/infinite-phpmyadmin.tar.gz /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite-phpmyadmin.tar.gz
-tar -zvxf /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite-phpmyadmin.tar.gz -C /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/
-rm -f /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite-phpmyadmin.tar.gz
-echo -n "$" >> /usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
-echo -n "cfg['ThemeDefault']= 'original';" >> /usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
+directory=/usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/infinite
+config_file=/usr/local/cpanel/base/3rdparty/phpMyAdmin/config.inc.php
+old="?>"
+new="$cfg['ThemeDefault']= 'original'; ?>"
+if [ ! -d "$directory" ]; then
+tar -zvxf /root/infinite-scripts/infinite-phpmyadmin.tar.gz -C /usr/local/cpanel/base/3rdparty/phpMyAdmin/themes/
+sed -r -i -e "s|$old|$new|g" $config_file;
 fi
