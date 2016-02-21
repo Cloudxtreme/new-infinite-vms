80c9247a38334660ac56c438debf33213b38cdf3
Infinite.sx <ops@shift.sx>
2016-02-21 20:31:13 -0300

    Update resize-main-disk.sh

--- resize-main-disk.sh
+++ resize-main-disk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
+yum update
 yum -y install epel-release
 yum -y install cloud-init cloud-initramfs-tools dracut-modules-growroot cloud-utils-growpart
 rpm -qa kernel | sed -e 's/^kernel-//' | xargs -I {} dracut -f /boot/initramfs-{}.img {}
