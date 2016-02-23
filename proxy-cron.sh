d227ae020afe92789d5f3837af9710986e34935d
Infinite.sx <ops@shift.sx>
2016-02-23 02:24:52 -0300

    Update proxy-cron.sh

--- proxy-cron.sh
+++ proxy-cron.sh
@@ -7,5 +7,5 @@ echo "00 22 */3 * * /usr/local/maldetect/maldet -d" | tee -a /var/spool/cron/roo
 echo "10 22 */3 * * /usr/local/maldetect/maldet -u" | tee -a /var/spool/cron/root
 echo "30 22 */3 * * /usr/local/maldetect/maldet -a" | sudo tee -a /var/spool/cron/root
 echo "00 23 1,15 * * (cd /root/chkrootkit-0.50; ./chkrootkit 2>&1)" | tee -a /var/spool/cron/root
-echo "*/2 * * * * sh /root/infinite-scripts/proxy-curl-check.sh >> /dev/null" | tee -a /var/spool/cron/root
+echo "*/1 * * * * sh /root/infinite-scripts/proxy-curl-check.sh >> /dev/null" | tee -a /var/spool/cron/root
 echo "30 2 1,15 * * yum clean all" | tee -a /var/spool/cron/root
