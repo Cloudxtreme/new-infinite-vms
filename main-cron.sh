4dd5361c89cc0d4e3f0129162cfec64dd47c77f0
Infinite.sx <ops@shift.sx>
2016-02-23 16:52:17 -0300

    Update main-cron.sh

--- main-cron.sh
+++ main-cron.sh
@@ -9,7 +9,6 @@ echo "0 22 1,15 * * (cd /root/chkrootkit-0.50; ./chkrootkit 2>&1)" | tee -a /var
 echo "*/2 * * * * /usr/local/sim/bin/sim -q >> /dev/null" | tee -a /var/spool/cron/root
 echo "7 0 * * * sh /root/infinite-scripts/whm-theme-changer.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "6 0 * * * sh /root/infinite-scripts/filemanager-theme-changer.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
-echo "5 0 * * * sh /root/infinite-scripts/phpMyAdmin-default-theme.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "0 4 * * * mysqlcheck -Aos -u root > /dev/null" | tee -a /var/spool/cron/root
 echo "30 0 * * 0 find /home/ -type f -name error_log -delete" | tee -a /var/spool/cron/root
 echo "0 0,7,13,19 * * * sync; echo 3 > /proc/sys/vm/drop_caches" | tee -a /var/spool/cron/root
