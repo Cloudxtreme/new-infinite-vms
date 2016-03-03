8d8ca8757d4abea2879ee643c7470782079b9ac5
Infinite.sx <ops@shift.sx>
2016-03-03 13:48:04 -0300

    Update proxy-csf.sh

--- proxy-csf.sh
+++ proxy-csf.sh
@@ -2,6 +2,7 @@
 sed -i 's/TESTING = "1"/TESTING = "0"/g' /etc/csf/csf.conf
 sed -i 's/RESTRICT_SYSLOG = "0"/RESTRICT_SYSLOG = "3"/g' /etc/csf/csf.conf
 sed -i 's/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,2221"/TCP_IN = "80,443,2221,2222"/g' /etc/csf/csf.conf
+sed -i 's/TCP_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP_OUT = "37,80,443"/g' /etc/csf/csf.conf
 sed -i 's/TCP_OUT = "20,21,22,25,37,53,80,110,113,443,587,993,995"/TCP_OUT = "37,80,443"/g' /etc/csf/csf.conf
 sed -i 's/UDP_IN = "20,21,53"/UDP_IN = ""/g' /etc/csf/csf.conf
 sed -i 's/UDP_OUT = "20,21,53,113,123"/UDP_OUT = ""/g' /etc/csf/csf.conf
