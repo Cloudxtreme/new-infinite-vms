e251b6f6a7416391ffae31a211ef392b15929c02
Infinite.sx <ops@shift.sx>
2016-03-03 13:29:58 -0300

    Update cloud-csf.sh

--- cloud-csf.sh
+++ cloud-csf.sh
@@ -9,7 +9,7 @@ sed -i 's/UDP_IN = "20,21,53"/UDP_IN = ""/g' /etc/csf/csf.conf
 sed -i 's/UDP_OUT = "20,21,53,113,123"/UDP_OUT = ""/g' /etc/csf/csf.conf
 sed -i 's/UDP_OUT = "20,21,53,113,123,873,6277,24441"/UDP_OUT = ""/g' /etc/csf/csf.conf
 sed -i 's/ICMP_IN = "1"/ICMP_IN = "0"/g' /etc/csf/csf.conf
-sed -i 's/IPV6 = "0"/IPV6 = "1"/g' /etc/csf/csf.conf
+sed -i 's/IPV6 = "1"/IPV6 = "0"/g' /etc/csf/csf.conf
 sed -i 's/IPV6_ICMP_STRICT = "0"/IPV6_ICMP_STRICT = "1"/g' /etc/csf/csf.conf
 sed -i 's/TCP6_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,2221"/TCP6_IN = ""/g' /etc/csf/csf.conf
 sed -i 's/TCP6_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,2077,2078,2082,2083,2086,2087,2095,2096,2222"/TCP6_IN = ""/g' /etc/csf/csf.conf
