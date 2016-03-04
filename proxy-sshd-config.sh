4a4dfd0a4b0f945e56052c369b9dc2e2c2478c46
Infinite.sx <ops@shift.sx>
2016-03-04 18:46:03 -0300

    Update proxy-sshd-config.sh

--- proxy-sshd-config.sh
+++ proxy-sshd-config.sh
@@ -7,4 +7,5 @@ sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/#GatewayPorts no/GatewayPorts yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/# %wheel/%wheel/g' /etc/sudoers
+sed -i 's/Defaults    requiretty/#Defaults    requiretty/g' /etc/sudoers
 /etc/init.d/sshd restart
