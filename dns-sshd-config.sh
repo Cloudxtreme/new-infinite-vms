89ef1ec7ff1a77f3f5665163a9fcfa10d20b1da6
Infinite.sx <ops@shift.sx>
2016-03-04 18:45:56 -0300

    Update dns-sshd-config.sh

--- dns-sshd-config.sh
+++ dns-sshd-config.sh
@@ -5,4 +5,5 @@ sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/# %wheel/%wheel/g' /etc/sudoers
+sed -i 's/Defaults    requiretty/#Defaults    requiretty/g' /etc/sudoers
 /etc/init.d/sshd restart
