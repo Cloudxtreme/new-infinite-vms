0d70a4e6adfd4802d19183c3680c09aaa392305e
Infinite.sx <ops@shift.sx>
2016-03-09 13:39:31 -0300

    Update mysql-governor.sh

--- mysql-governor.sh
+++ mysql-governor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install governor-mysql
-/usr/share/lve/dbgovernor/db-select-mysql --mysql-version=auto
+/usr/share/lve/dbgovernor/db-select-mysql --mysql-version=mariadb101
 /usr/share/lve/dbgovernor/mysqlgovernor.py --install
