d1528ba7ad073b1120db38dc82c8b21873f4be73
Infinite.sx <ops@shift.sx>
2016-03-14 00:02:20 -0300

    Update proxy-curl-check.sh

--- proxy-curl-check.sh
+++ proxy-curl-check.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 nginx_conf=/etc/nginx/nginx.conf
+nginx_ssl_conf=/etc/nginx/sites-enabled/ssls.conf
 curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 20)
 curl_result=200
 main_proxy_pass="proxy_pass http://ip-cloud;"
+main_ssl_proxy_pass="proxy_pass https://ip-cloud;"
 cp_proxy_pass="proxy_pass http://cpanel-webserver;"
 main_return="return 503; #main"
 cp_return="return 503; #cp"
@@ -10,7 +12,9 @@ cp_return="return 503; #cp"
 if [ "$curl_check" != "$curl_result" ]; then
 grep "$main_proxy_pass" $nginx_conf && sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf && service nginx restart
 grep "$cp_proxy_pass" $nginx_conf && sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf && service nginx restart
+grep "$main_ssl_proxy_pass" $nginx_ssl_conf && sed -r -i -e "s|$main_ssl_proxy_pass|$main_return|g" $nginx_ssl_conf && service nginx restart
 else
 grep "$main_return" $nginx_conf && sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_conf && service nginx restart
 grep "$cp_return" $nginx_conf && sed -r -i -e "s|$cp_return|$cp_proxy_pass|g" $nginx_conf && service nginx restart
+grep "$main_return" $nginx_ssl_conf && sed -r -i -e "s|$main_return|$main_ssl_proxy_pass|g" $nginx_ssl_conf && service nginx restart
 fi
