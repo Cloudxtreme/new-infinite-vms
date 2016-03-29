22801c6dca0289c2aa43a89fb5ae7cb1bd30bb4b
Infinite.sx <ops@shift.sx>
2016-03-29 13:13:02 -0300

    Update proxy-curl-check.sh

--- proxy-curl-check.sh
+++ proxy-curl-check.sh
@@ -10,11 +10,11 @@ main_return="return 503; #main"
 cp_return="return 503; #cp"
 
 if [ "$curl_check" != "$curl_result" ]; then
-grep "$main_proxy_pass" $nginx_conf && sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf && service nginx restart
-grep "$cp_proxy_pass" $nginx_conf && sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf && service nginx restart
+grep "$main_proxy_pass" $nginx_conf && sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf
+grep "$cp_proxy_pass" $nginx_conf && sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf
 grep "$main_ssl_proxy_pass" $nginx_ssl_conf && sed -r -i -e "s|$main_ssl_proxy_pass|$main_return|g" $nginx_ssl_conf && service nginx restart
 else
-grep "$main_return" $nginx_conf && sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_conf && service nginx restart
-grep "$cp_return" $nginx_conf && sed -r -i -e "s|$cp_return|$cp_proxy_pass|g" $nginx_conf && service nginx restart
+grep "$main_return" $nginx_conf && sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_conf
+grep "$cp_return" $nginx_conf && sed -r -i -e "s|$cp_return|$cp_proxy_pass|g" $nginx_conf
 grep "$main_return" $nginx_ssl_conf && sed -r -i -e "s|$main_return|$main_ssl_proxy_pass|g" $nginx_ssl_conf && service nginx restart
 fi
