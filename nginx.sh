a6c8ce9359805afefc83751b3c48e8e4619aa8c1
Infinite.sx <ops@shift.sx>
2016-03-30 12:36:16 -0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,11 +2,13 @@
 rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
 wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
 yum -y install gcc-c++ pcre-devel pcre-devel zlib-devel make unzip openssl-devel devtoolset-2-gcc-c++ devtoolset-2-binutils
-nginxVersion="1.8.1"
+service nginx stop
+nginxVersion="1.9.13"
 NPS_VERSION="1.10.33.7"
 PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
 
 # PageSpeed
+rm -rf /opt/nginx
 mkdir -p /opt/nginx/modules
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip -O /opt/nginx/modules/release-${NPS_VERSION}-beta.zip
 unzip /opt/nginx/modules/release-${NPS_VERSION}-beta.zip
@@ -14,6 +16,7 @@ wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz -O /opt/ngin
 tar -xzf /opt/nginx/modules/ngx_pagespeed-release-${NPS_VERSION}-beta/${NPS_VERSION}.tar.gz
 
 # NGINX
+rm -rf /src/nginx
 cd /
 mkdir -p /src
 wget http://nginx.org/download/nginx-$nginxVersion.tar.gz -O /src/nginx-$nginxVersion.tar.gz
@@ -33,7 +36,8 @@ cd /src/nginx && ./configure --add-module=/opt/nginx/modules/ngx_pagespeed-relea
 --with-http_gzip_static_module \
 --with-http_auth_request_module \
 --with-file-aio \
---with-http_spdy_module \
+--with-threads \
+--with-http_v2_module \
 --with-pcre \
 --without-mail_pop3_module \
 --without-mail_imap_module \
