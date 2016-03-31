885c0f0062c87cab0ae86ea145391e0e813b77eb
Infinite.sx <ops@shift.sx>
2016-03-31 13:54:29 -0300

    Update sysctl-config.sh

--- sysctl-config.sh
+++ sysctl-config.sh
@@ -33,6 +33,22 @@ echo "kernel.sched_migration_cost_ns = 2500000" >> /etc/sysctl.conf
 echo "kernel.exec-shield= 1" >> /etc/sysctl.conf
 echo "kernel.randomize_va_space= 1" >> /etc/sysctl.conf
 echo "fs.file-max = 200000" >> /etc/sysctl.conf
+echo "net.core.rmem_max=8388608" >> /etc/sysctl.conf
+echo "net.core.wmem_max=8388608" >> /etc/sysctl.conf
+echo "net.core.rmem_default=65536" >> /etc/sysctl.conf
+echo "net.core.wmem_default=65536" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem=8192 873800 8388608" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem=4096 655360 8388608" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_mem=8388608 8388608 8388608" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_tw_buckets=6000000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_syn_backlog=65536" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_orphans=262144" >> /etc/sysctl.conf
+echo "net.core.somaxconn = 16384" >> /etc/sysctl.conf
+echo "net.core.netdev_max_backlog = 16384" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_slow_start_after_idle = 0" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 3000 65000" >> /etc/sysctl.conf
 sysctl -e -p
 service cups stop
 chkconfig cups off
