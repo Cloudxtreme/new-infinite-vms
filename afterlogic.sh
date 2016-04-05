ed0670c27b69b4d432a783fdfab0de1abe92bcf3
Infinite.sx <ops@shift.sx>
2016-04-05 16:14:39 -0300

    Update afterlogic.sh

--- afterlogic.sh
+++ afterlogic.sh
@@ -8,3 +8,4 @@ chmod a+x ./installer
 tar -zvxf /root/infinite-scripts/afterlogic-infinite.tar.gz -C /usr/local/cpanel/base/3rdparty/afterlogic/skins/
 rm -f /root/infinite-scripts/afterlogic-infinite.tar.gz
 sed -i '$i'\''themes'\'' => array('\''Original'\''),' /usr/local/cpanel/base/3rdparty/afterlogic/data/settings/config.php
+sed -i 's/X-Priority/X-Prioridade/g' /usr/local/cpanel/base/3rdparty/afterlogic/libraries/MailSo/Mime/Enumerations/Header.php
