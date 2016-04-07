8414eb9b9ec1ba3b4024e02a8608dd93adea7e61
Infinite.sx <ops@shift.sx>
2016-04-07 00:21:06 -0300

    Update dns-cron.sh

--- dns-cron.sh
+++ dns-cron.sh
@@ -5,7 +5,7 @@ chkconfig crond on
 touch /var/spool/cron/root
 echo "00 22 */3 * * /usr/local/maldetect/maldet -d > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "10 22 */3 * * /usr/local/maldetect/maldet -u > /dev/null 2>&1" | tee -a /var/spool/cron/root
-echo "30 22 */3 * * /usr/local/maldetect/maldet -a > /dev/null 2>&1" | sudo tee -a /var/spool/cron/root
+echo "30 22 */3 * * /usr/local/maldetect/maldet -a > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "00 23 1,15 * * (cd /root/chkrootkit-0.50; ./chkrootkit 2>&1)" | tee -a /var/spool/cron/root
 echo "*/1 * * * * sh /root/infinite-scripts/dns-curl-check.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "0 */1 * * * sh /root/infinite-scripts/dns-curl-check-bk.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
