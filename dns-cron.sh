b5a02bb8aaf178d71f805f5aa168d36603f47459
Infinite.sx <ops@shift.sx>
2016-04-08 18:30:24 -0300

    Update dns-cron.sh

--- dns-cron.sh
+++ dns-cron.sh
@@ -7,7 +7,7 @@ echo "00 22 */3 * * /usr/local/maldetect/maldet -d > /dev/null 2>&1" | tee -a /v
 echo "10 22 */3 * * /usr/local/maldetect/maldet -u > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "30 22 */3 * * /usr/local/maldetect/maldet -a > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "00 23 1,15 * * (cd /root/chkrootkit-0.50; ./chkrootkit 2>&1)" | tee -a /var/spool/cron/root
-echo "*/1 * * * * sh /root/infinite-scripts/dns-curl-check.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
-echo "0 */1 * * * sh /root/infinite-scripts/dns-curl-check-bk.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
+echo "*/2 * * * * sh /root/infinite-scripts/dns-curl-check.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
+echo "0 */2 * * * sh /root/infinite-scripts/dns-curl-check-bk.sh > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "30 2 1,15 * * yum clean all > /dev/null 2>&1" | tee -a /var/spool/cron/root
 echo "30 0 * * * rm -f /var/named/sed* > /dev/null 2>&1" | tee -a /var/spool/cron/root
