85e1ba76f1502d827d8b3a9dfbc3f446de1edfaa
Infinite.sx <ops@shift.sx>
2016-04-07 23:43:03 -0300

    Update proxy-curl-check.sh

--- proxy-curl-check.sh
+++ proxy-curl-check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 nginx_conf=/etc/nginx/nginx.conf
 nginx_ssl_conf=/etc/nginx/sites-enabled/ssls.conf
-first_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/cgi-sys/defaultwebpage.cgi -m 20)
-second_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 30)
+first_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/cgi-sys/defaultwebpage.cgi -m 30)
+second_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 20)
+third_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 10)
 curl_result=200
 main_proxy_pass="proxy_pass http://ip-cloud;"
 main_ssl_proxy_pass="proxy_pass https://ip-cloud;"
@@ -10,7 +11,7 @@ cp_proxy_pass="proxy_pass http://cpanel-webserver;"
 main_return="return 503; #main"
 cp_return="return 503; #cp"
 
-if [ "$first_curl_check" != "$curl_result" ] && [ "$second_curl_check" != "$curl_result" ]; then
+if [ "$first_curl_check" != "$curl_result" ] && [ "$second_curl_check" != "$curl_result" ] && [ "$third_curl_check" != "$curl_result" ]; then
 grep "$main_proxy_pass" $nginx_conf && sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf
 grep "$cp_proxy_pass" $nginx_conf && sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf
 grep "$main_ssl_proxy_pass" $nginx_ssl_conf && sed -r -i -e "s|$main_ssl_proxy_pass|$main_return|g" $nginx_ssl_conf && service nginx restart
