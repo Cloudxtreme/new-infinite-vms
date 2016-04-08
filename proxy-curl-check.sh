e3c9aaf25e1b698e3a63fbf5d742dd13aed0f49a
Infinite.sx <ops@shift.sx>
2016-04-08 02:16:47 -0300

    Update proxy-curl-check.sh

--- proxy-curl-check.sh
+++ proxy-curl-check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 nginx_conf=/etc/nginx/nginx.conf
+nginx_ssl_location_conf=/etc/nginx/ssl-location.conf
 nginx_ssl_conf=/etc/nginx/sites-enabled/ssls.conf
 first_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/cgi-sys/defaultwebpage.cgi -m 30)
 second_curl_check=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://ip-cloud/ -m 20)
@@ -15,9 +16,11 @@ cp_return="return 503; #cp"
 if [ "$first_curl_check" != "$curl_result" ] && [ "$second_curl_check" != "$curl_result" ] && [ "$ssl_curl_check" != "$ssl_curl_result" ]; then
 grep "$main_proxy_pass" $nginx_conf && sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_conf
 grep "$cp_proxy_pass" $nginx_conf && sed -r -i -e "s|$cp_proxy_pass|$cp_return|g" $nginx_conf
-grep "$main_ssl_proxy_pass" $nginx_ssl_conf && sed -r -i -e "s|$main_ssl_proxy_pass|$main_return|g" $nginx_ssl_conf && service nginx restart
+grep "$main_proxy_pass" $nginx_ssl_conf && sed -r -i -e "s|$main_proxy_pass|$main_return|g" $nginx_ssl_conf
+grep "$main_ssl_proxy_pass" $nginx_ssl_location_conf && sed -r -i -e "s|$main_ssl_proxy_pass|$main_return|g" $nginx_ssl_location_conf  && service nginx restart
 else
 grep "$main_return" $nginx_conf && sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_conf
 grep "$cp_return" $nginx_conf && sed -r -i -e "s|$cp_return|$cp_proxy_pass|g" $nginx_conf
-grep "$main_return" $nginx_ssl_conf && sed -r -i -e "s|$main_return|$main_ssl_proxy_pass|g" $nginx_ssl_conf && service nginx restart
+grep "$main_return" $nginx_ssl_conf && sed -r -i -e "s|$main_return|$main_proxy_pass|g" $nginx_ssl_conf 
+grep "$main_return" $nginx_ssl_location_conf && sed -r -i -e "s|$main_return|$main_ssl_proxy_pass|g" $nginx_ssl_location_conf && service nginx restart
 fi
