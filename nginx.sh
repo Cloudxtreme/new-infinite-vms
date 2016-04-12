2fdea826b6311592b3d054fb0aaa242b4279a6dc
Infinite.sx <ops@shift.sx>
2016-04-12 12:05:48 -0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,8 +3,8 @@ rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/sl
 wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
 yum -y install gcc-c++ pcre-devel pcre-devel zlib-devel make unzip openssl-devel devtoolset-2-gcc-c++ devtoolset-2-binutils
 service nginx stop
-nginxVersion="1.9.13"
-NPS_VERSION="1.10.33.7"
+nginxVersion="1.9.14"
+NPS_VERSION="1.10.33.0"
 PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
 
 # PageSpeed
