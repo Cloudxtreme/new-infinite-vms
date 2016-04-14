d9cb6a93f5238b802ed354fab7bf8c524b725a36
Infinite.sx <ops@shift.sx>
2016-04-14 02:34:07 -0300

    Update lsapi.sh

--- lsapi.sh
+++ lsapi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum install -y liblsapi liblsapi-devel --enablerepo=cloudlinux-updates-testing
 yum install -y cpanel-mod-lsapi --enablerepo=cloudlinux-updates-testing
+/usr/bin/switch_mod_lsapi --build-native-lsphp
 /usr/bin/switch_mod_lsapi --setup
 /usr/bin/switch_mod_lsapi --enable-global
 sed -i 's/# lsapi_terminate_backends_on_exit On/lsapi_terminate_backends_on_exit Off/g' /usr/local/apache/conf/conf.d/lsapi.conf
