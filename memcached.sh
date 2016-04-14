0ba36adc7a03a767890a54d6d7eefd93ec48b517
Infinite.sx <ops@shift.sx>
2016-04-14 03:26:56 -0300

    Update memcached.sh

--- memcached.sh
+++ memcached.sh
@@ -3,4 +3,5 @@ yum install memcached libmemcached
 service memcached start
 chkconfig --levels 235 memcached on
 /usr/local/bin/pecl install memcache
+echo "extension=memcache.so" >> /usr/local/lib/php.ini
 service httpd restart
