ccc191ad00d8f934237e2db27d6890f01c1223c1
Infinite.sx <infinitesx@users.noreply.github.com>
2016-04-29 13:55:48 -0300

    Create img-optimizer.sh

---
+++ img-optimizer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find /home/*/public_html/ -type f \( ! -name "*-raw.png" -and -name "*.png"  \) -print0 | xargs -0 -P2 -L1 pngquant --quality=80-90 --speed 6 --ext .png --force --skip-if-larger
+find /home/*/public_html/ -type f \( ! -name "*-raw.jpg" -and -name "*.jpg"  \) -print0 | xargs -0 -P2 -L1 jpegoptim -qP --strip-icc --strip-xmp --strip-com -m 90
