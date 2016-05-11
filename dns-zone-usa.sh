762c65a813c875eb0eea851d7ecf55640376d84e
Infinite.sx <infinitesx@users.noreply.github.com>
2016-05-11 17:50:08 -0300

    Delete dns-zone-usa.sh

--- dns-zone-usa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mv -f /root/infinite-scripts/usa/root_simple /var/cpanel/zonetemplates/root_simple
-mv -f /root/infinite-scripts/usa/root_standard /var/cpanel/zonetemplates/root_standard
-mv -f /root/infinite-scripts/usa/root_standardvirtualftp /var/cpanel/zonetemplates/root_standardvirtualftp
-rmdir /root/infinite-scripts/usa
