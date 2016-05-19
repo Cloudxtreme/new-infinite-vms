b826cc0a6bacd2673f904a58fb4f478c3d82cbf9
Infinite.sx <infinitesx@users.noreply.github.com>
2016-05-19 02:15:10 -0300

    Update clean-logs.sh

--- clean-logs.sh
+++ clean-logs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 find /var/log -name "secure*" -type f -exec rm -rf {} \;
+find /var/log -name "exim*" -type f -exec rm -rf {} \;
+find /var/log -name "sim_log*" -type f -exec rm -rf {} \;
+find /var/log -name "cron*" -type f -exec rm -rf {} \;
+find /var/log -name "maillog*" -type f -exec rm -rf {} \;
 history -cw
